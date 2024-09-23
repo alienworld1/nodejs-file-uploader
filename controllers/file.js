2bd470858f5a995084fdede6c0811cde50673f16
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-09-23 19:41:33 +0530

    Add download files feature

--- controllers/file.js
+++ controllers/file.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const path = require('path');
 const { DateTime } = require('luxon');
 const { PrismaClient } = require('@prisma/client');
 
@@ -20,7 +21,7 @@ exports.getFileDetails = asyncHandler(async (req, res, next) => {
     return next(err);
   }
 
-  if (file.userId != req.user.id) {
+  if (file.userId !== req.user.id) {
     const err = new Error('Not authorized to view the file');
     err.status = 403;
     return next(err);
@@ -28,4 +29,31 @@ exports.getFileDetails = asyncHandler(async (req, res, next) => {
 
   const formattedDate = DateTime.fromJSDate(file.createdAt).toLocaleString();
   res.render('file_view', { title: file.name, file, formattedDate });
+});
+
+exports.downloadFile = asyncHandler(async (req, res, next) => {
+  const { fileid } = req.params;
+  const fileId = parseInt(fileid);
+
+  const file = await prisma.file.findUnique({
+    where: {
+      id: fileId,
+    },
+  });
+
+  if (!file) {
+    const err = new Error('File not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  if (file.userId !== req.user.id) {
+    const err = new Error('Not authorized to view the file');
+    err.status = 403;
+    return next(err);
+  }
+
+  // This header is used to determine the filename that is downloaded
+  res.set('Content-Disposition', `attachment; filename="${file.name}"`);
+  res.sendFile(path.resolve(__dirname, '..', file.path));
 });
--- routes/file.js
+++ routes/file.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const fileController = require('../controllers/file');
 
 router.get('/:fileid', fileController.getFileDetails);
+router.get('/:fileid/download', fileController.downloadFile);
 
 module.exports = router;
