bfdab6dc9c184d95bbfb971d25344b3d40eb34d9
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-09-23 21:55:34 +0530

    Add rename file page

--- controllers/file.js
+++ controllers/file.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const path = require('path');
 const { DateTime } = require('luxon');
 const { PrismaClient } = require('@prisma/client');
+const { body, validationResult } = require('express-validator');
 
 const prisma = new PrismaClient();
 
@@ -57,3 +58,88 @@ exports.downloadFile = asyncHandler(async (req, res, next) => {
   res.set('Content-Disposition', `attachment; filename="${file.name}"`);
   res.sendFile(path.resolve(__dirname, '..', file.path));
 });
+
+exports.renameGet = asyncHandler(async (req, res, next) => {
+  const { fileid } = req.params;
+  const fileId = parseInt(fileid);
+
+  const file = await prisma.file.findUnique({
+    where: {
+      id: fileId,
+    },
+    select: {
+      name: true,
+      userId: true,
+    },
+  });
+
+  if (!file) {
+    const err = new Error('File not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  if (file.userId !== req.user.id) {
+    const err = new Error('Not authorized to view the file');
+    err.status = 403;
+    return next(err);
+  }
+
+  res.render('file_rename', { title: 'Rename File', file });
+});
+
+exports.renamePost = [
+  body('filename')
+    .trim()
+    .isLength({ min: 1, max: 32 })
+    .withMessage('Filename length should be between 1-32 characters')
+    .matches(/^[\w\-. ]+$/)
+    .withMessage('A valid filename is required'),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const { fileid, folderid } = req.params;
+    const fileId = parseInt(fileid);
+
+    const file = await prisma.file.findUnique({
+      where: {
+        id: fileId,
+      },
+      select: {
+        name: true,
+        userId: true,
+      },
+    });
+
+    if (!file) {
+      const err = new Error('File not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    if (file.userId !== req.user.id) {
+      const err = new Error('Not authorized to view the file');
+      err.status = 403;
+      return next(err);
+    }
+
+    if (!errors.isEmpty()) {
+      res.render('file_rename', {
+        title: 'Rename File',
+        file,
+        error: errors.array()[0],
+      });
+      return;
+    }
+
+    await prisma.file.update({
+      where: {
+        id: fileId,
+      },
+      data: {
+        name: req.body.filename,
+      },
+    });
+
+    res.redirect('/');
+  }),
+];
--- routes/file.js
+++ routes/file.js
@@ -4,4 +4,7 @@ const fileController = require('../controllers/file');
 router.get('/:fileid', fileController.getFileDetails);
 router.get('/:fileid/download', fileController.downloadFile);
 
+router.get('/:fileid/rename', fileController.renameGet);
+router.post('/:fileid/rename', fileController.renamePost);
+
 module.exports = router;
